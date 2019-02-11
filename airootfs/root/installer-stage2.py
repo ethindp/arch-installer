618d7020c0ef38da5c8e009496e4932e4db8ba05
Ethin Probst <harlydavidsen@gmail.com>
2019-02-11 09:41:21 -0600

    Refactored the code and made the code more compliant with PEP8.

--- airootfs/root/installer-stage2.py
+++ airootfs/root/installer-stage2.py
@@ -1,241 +1,244 @@
 import sys
 import os
 import subprocess
-import socket
 import shlex
-import json
 import shutil
 import platform
 import tempfile
 
+
 # executes the given command, but does not display output.
-def run(command: str, rshell: bool=False):
-	proc=subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
-	if proc.returncode!=0: # assume failure
-		print ("The following command failed when executed:")
-		print(command)
-		if click.confirm("Would you like to view the processes output?"):
-			print(proc.stdout.decode())
-			print(proc.stderr.decode())
-		if not click.confirm("Would you like to continue the installation process?"):
-			sys.exit(0)
+def run(command: str, rshell: bool = False):
+    proc = subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
+    if proc.returncode != 0:  # assume failure
+        print("The following command failed when executed:")
+        print(command)
+        if click.confirm("Would you like to view the processes output?"):
+            print(proc.stdout.decode())
+            print(proc.stderr.decode())
+        if not click.confirm("Would you like to continue the installation process?"):
+            sys.exit(0)
+
 
 # Does same thing as run() but returns output
-def execute(command: str, rshell: bool=False)->tuple:
-	proc=subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
-	if proc.returncode!=0: # assume failure
-		print ("The following command failed when executed:")
-		print(command)
-		if click.confirm("Would you like to view the processes output?"):
-			print(proc.stdout.decode())
-			print(proc.stderr.decode())
-		if not click.confirm("Would you like to continue the installation process?"):
-			sys.exit(0)
-	else:
-		return (proc.stdout.decode(), proc.stderr.decode())
+def execute(command: str, rshell: bool = False) -> tuple:
+    proc = subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
+    if proc.returncode != 0:  # assume failure
+        print("The following command failed when executed:")
+        print(command)
+        if click.confirm("Would you like to view the processes output?"):
+            print(proc.stdout.decode())
+            print(proc.stderr.decode())
+        if not click.confirm("Would you like to continue the installation process?"):
+            sys.exit(0)
+            return (None, None)
+    else:
+        return (proc.stdout.decode(), proc.stderr.decode())
 
-print ("Installing required python modules")
+
+print("Installing required python modules")
 run("pip install console-menu click netifaces")
-from consolemenu import *
-from consolemenu.items import *
+from consolemenu import SelectionMenu
 import click
 import netifaces
-de_packages=[]
-dm=-1
+
+
+DE_PACKAGES = []
+DM = -1
 if click.confirm("Would you like to install a desktop environment?"):
-	menu=SelectionMenu(['Gnome', 'LXDE (GTK2)', 'LXDE (GTK3)', Mate'], title="Select a desktop environment to install")
-	menu.show(False)
-	selection=menu.selected_item.index
-	dm=menu.selected_item.index
-	de_packages.extend(["xorg", "xorg-apps", "xorg-drivers", "xorg-fonts"])
-	if selection==0:
-		de_packages.extend(["gnome", "gnome-extra", "orca"])
-	elif selection==1:
-		de_packages.extend(["lxde", "orca"])
-	elif selection==2:
-		de_packages.extend(["lxde-gtk3", "orca"])
-	elif selection==3:
-		de_packages.extend(["mate", "mate-extra", "orca"])
-	de_packages.sort()
-	print ("Installing selected desktop environment")
-	run(f"""pacman -Syu {" ".join(de_packages)} --noconfirm""")
+    MENU = SelectionMenu(['Gnome', 'LXDE (GTK2)', 'LXDE (GTK3)', 'Mate'], title="Select a desktop environment to install")
+    MENU.show(False)
+    SELECTION = MENU.selected_item.index
+    DM = MENU.selected_item.index
+    DE_PACKAGES.extend(["xorg", "xorg-apps", "xorg-drivers", "xorg-fonts"])
+    if SELECTION == 0:
+        DE_PACKAGES.extend(["gnome", "gnome-extra", "orca"])
+    elif SELECTION == 1:
+        DE_PACKAGES.extend(["lxde", "orca"])
+    elif SELECTION == 2:
+        DE_PACKAGES.extend(["lxde-gtk3", "orca"])
+    elif SELECTION == 3:
+        DE_PACKAGES.extend(["mate", "mate-extra", "orca"])
+    DE_PACKAGES.sort()
+    print("Installing selected desktop environment")
+    run(f"""pacman -Syu {" ".join(DE_PACKAGES)} --noconfirm""")
 
 if click.confirm("Would you like to add any other packages to the system?"):
-	print("Enter all packages separated by a space.")
-	packages=click.prompt("Packages to add")
-	print (f"Installing {shlex.split(packages)} packages...")
-	run(f"pacman -Syu {packages} --noconfirm")
+    print("Enter all packages separated by a space.")
+    PACKAGES = click.prompt("Packages to add")
+    print(f"Installing {shlex.split(PACKAGES)} packages...")
+    run(f"pacman -Syu {PACKAGES} --noconfirm")
 
-print ("Setting timezone to default")
+print("Setting timezone to default")
 if os.path.exists("/etc/localtime"):
-	os.remove("/etc/localtime")
+    os.remove("/etc/localtime")
 run("ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime")
 run("hwclock --systohc")
 while True:
-	print ("These are the settings for your current time/date configuration:")
-	print(execute("timedatectl")[0])
-	if click.confirm("Would you like to change them?"):
-		tzs=execute("timedatectl list-timezones")[0].split()
-		fd, fname=tempfile.mkstemp(text=True)
-		with os.fdopen(fd, "w") as f:
-			f.write("The following timezones are available:\n")
-			for count, tz in enumerate(tzs):
-				f.write(f"{count}: {tz}\n")
-   f.flush()
-      os.fsync(f.fileno())
-		os.close(fd)
-		subprocess.run(f"less -- {fname}")
-		while True:
-			tzid=click.prompt(f"Enter timezone number (1-{len(tzs)}")
-			if tzid<1 or tzid>len(tzs):
-				print ("Error: invalid timezone number")
-				continue
-			else:
-				run(f"timedatectl set-timezone {tzs[tzid-1]}")
-				break
-	else:
-		break
+    print("These are the settings for your current time/date configuration:")
+    print(execute("timedatectl")[0])
+    if click.confirm("Would you like to change them?"):
+        TZS = execute("timedatectl list-timezones")[0].split()
+        FD, FNAME = tempfile.mkstemp(text=True)
+        with os.fdopen(FD, "w") as f:
+            f.write("The following timezones are available:\n")
+            for count, tz in enumerate(TZS):
+                f.write(f"{count}: {tz}\n")
+            f.flush()
+            os.fsync(f.fileno())
+        os.close(FD)
+        subprocess.run(f"less -- {FNAME}")
+        while True:
+            TZID = click.prompt(f"Enter timezone number (1-{len(TZS)}")
+            if TZID < 1 or TZID > len(TZS):
+                print("Error: invalid timezone number")
+                continue
+            else:
+                run(f"timedatectl set-timezone {TZS[TZID-1]}")
+                break
+    else:
+        break
 
-print ("Setting and generating locale")
+print("Setting and generating locale")
 run("sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen")
 run("locale-gen")
 with open("/etc/locale.conf", "w") as f:
-	f.write("LANG=en_US.UTF-8")
+    f.write("LANG=en_US.UTF-8")
 
 if click.confirm("Would you like to set a system hostname?"):
-	hostname=click.prompt("Enter your hostname")
-	with open("/etc/hostname", "w") as f:
-		f.write(hostname)
+    HOSTNAME = click.prompt("Enter your hostname")
+    with open("/etc/hostname", "w") as f:
+        f.write(HOSTNAME)
 
-print ("Please specify the root password when prompted:")
+print("Please specify the root password when prompted:")
 subprocess.run("passwd")
 if click.confirm("Would you like to enable microcode updates for this system?"):
-	if platform.processor().lower().find("intel"):
-		print ("Enabling Intel microcode updates")
-		run("pacman -S intel-ucode --noconfirm")
-	elif platform.processor().lower().find("amd"):
-		print ("Enabling AMD microcode updates")
-		run("pacman -S amd-ucode --noconfirm")
-	else:
-		print ("Processor does not need microcode updates, skipping")
+    if platform.processor().lower().find("intel"):
+        print("Enabling Intel microcode updates")
+        run("pacman -S intel-ucode --noconfirm")
+    elif platform.processor().lower().find("amd"):
+        print("Enabling AMD microcode updates")
+        run("pacman -S amd-ucode --noconfirm")
+    else:
+        print("Processor does not need microcode updates, skipping")
 
-print ("Installing boot loader")
+print("Installing boot loader")
 if os.path.exists("/sys/firmware/efi") or os.path.exists("/sys/firmware/efi/efivars") or os.path.exists("/sys/firmware/efi/vars"):
-	if platform.architecture()[0]=="64bit":
-		run("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB")
-	elif platform.architecture()[0]=="32bit":
-		run("grub-install --target=i386-efi --efi-directory=/boot --bootloader-id=GRUB")
-	else:
-		print("Error: unknown CPU architecture!")
-		sys.exit(0)
+    if platform.architecture()[0] == "64bit":
+        run("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB")
+    elif platform.architecture()[0] == "32bit":
+        run("grub-install --target=i386-efi --efi-directory=/boot --bootloader-id=GRUB")
+    else:
+        print("Error: unknown CPU architecture!")
+        sys.exit(0)
 else:
-	run(f"grub-install --target=i386-pc {sys.argv[1]}")
+    run(f"grub-install --target=i386-pc {sys.argv[1]}")
 
 if click.confirm("Would you like to modify the grub configuration file (/etc/default/grub)?"):
-	print ("Opening grub configuration file. When done, save with ctrl+x.")
-	print ("The installation procedure will continue as soon as Nano is closed.")
-	subprocess.run(shlex.split("nano /etc/default/grub"))
-print ("Generating boot loader configuration")
+    print("Opening grub configuration file. When done, save with ctrl+x.")
+    print("The installation procedure will continue as soon as Nano is closed.")
+    subprocess.run(shlex.split("nano /etc/default/grub"))
+print("Generating boot loader configuration")
 run("grub-mkconfig -o /boot/grub/grub.cfg")
-print ("Creating administrative user accounts")
-users=[]
+print("Creating administrative user accounts")
+USERS = []
 while True:
-	username=click.prompt("Username")
-	proc=subprocess.run(f"useradd -m -g users -G power,storage,wheel -s /bin/bash {username}")
-	if proc.returncode!=0:
-		print ("Error: invalid username specification; user was not added to system. Please try again.")
-		continue
-	else:
-		subprocess.call(["passwd", username])
-		users.append(username)
-		if not click.confirm("Add another?"):
-			break
+    USERNAME = click.prompt("Username")
+    PROC = subprocess.run(f"useradd -m -g users -G power,storage,wheel -s /bin/bash {USERNAME}")
+    if PROC.returncode != 0:
+        print("Error: invalid username specification; user was not added to system. Please try again.")
+        continue
+    else:
+        subprocess.call(["passwd", USERNAME])
+        USERS.append(USERNAME)
+        if not click.confirm("Add another?"):
+            break
 
 if click.confirm("Modify sudoers file for administrator accounts?"):
-	print ("Opening sudoers file in Nano. When done, press ctrl+x.")
-	print ("The installation will continue immediately thereafter.")
-	os.environ["EDITOR"]="nano"
-	subprocess.run(["visudo"])
-if len(de_packages)>0:
-	if click.confirm("Would you like to boot into a display manager/desktop environment?")
-		if dm==0:
-			print ("Enabling GDM as display manager")
-			run("systemctl enable gdm")
-		elif dm==1 or dm==2:
-			print ("Enabling LXDM as display manager")
-			run("systemctl enable lxdm")
-		elif dm==3:
-			print ("Enabling LightDM as display manager")
-			run("pacman -Syu lightdm --noconfirm")
-			run("systemctl enable lightdm")
+    print("Opening sudoers file in Nano. When done, press ctrl+x.")
+    print("The installation will continue immediately thereafter.")
+    os.environ["EDITOR"] = "nano"
+    subprocess.run(["visudo"])
+if DE_PACKAGES:
+    if click.confirm("Would you like to boot into a display manager/desktop environment?"):
+        if DM == 0:
+            print("Enabling GDM as display manager")
+            run("systemctl enable gdm")
+        elif DM in (1, 2):
+            print("Enabling LXDM as display manager")
+            run("systemctl enable lxdm")
+        elif DM == 3:
+            print("Enabling LightDM as display manager")
+            run("pacman -Syu lightdm --noconfirm")
+            run("systemctl enable lightdm")
 
 if click.confirm("Would you like to enable orca accessibility?"):
-	print ("Enabling orca accessibility for all non-root users")
-	run("pacman -Syu orca --noconfirm")
-	run("gsettings set org.gnome.desktop.interface toolkit-accessibility true")
-	for user in users:
-		run(f"runuser --user {user} -- gsettings set org.mate.interface accessibility true")
-	run(f"runuser --user {user} -- gsettings set org.gnome.desktop.a11y.applications screen-reader-enabled true")
+    print("Enabling orca accessibility for all non-root users")
+    run("pacman -Syu orca --noconfirm")
+    run("gsettings set org.gnome.desktop.interface toolkit-accessibility true")
+    for user in USERS:
+        run(f"runuser --user {user} -- gsettings set org.mate.interface accessibility true")
+        run(f"runuser --user {user} -- gsettings set org.gnome.desktop.a11y.applications screen-reader-enabled true")
 
 if click.confirm("Would you like to enable AUR support?"):
-	print ("Enabling AUR support with Yay")
-	run("pacman -U /yay-9.1.0-1-x86_64.pkg.tar.xz --noconfirm")
-	os.remove("/yay-9.1.0-1-x86_64.pkg.tar.xz")
+    print("Enabling AUR support with Yay")
+    run("pacman -U /yay-9.1.0-1-x86_64.pkg.tar.xz --noconfirm")
+    os.remove("/yay-9.1.0-1-x86_64.pkg.tar.xz")
 else:
-	os.remove("/yay-9.1.0-1-x86_64.pkg.tar.xz")
+    os.remove("/yay-9.1.0-1-x86_64.pkg.tar.xz")
 
 if click.confirm("Would you like to enable a console screen reader? This will disable the currently enabled display manager."):
-	menu=SelectionMenu(["Speakup", "Fenrir"], title="Select a console screen reader to enable")
-	menu.show(False)
-	if menu.selected_item.index==0:
-		print ("Enabling speakup")
-		print ("Installing espeakup and speakup-utils")
-		run("pacman -Syu espeakup speakup-utils --noconfirm")
-		print ("Disabling display manager")
-		if dm==0:
-			run("systemctl disable gdm")
-		elif dm==1 or dm==2:
-			run("systemctl disable lxdm")
-		elif dm==3:
-			run("systemctl disable lightdm")
-		print ("Enabling espeakup service")
-		run("systemctl enable espeakup")
-	elif menu.selected_item.index==1:
-		print ("Enabling fenrir")
-		print ("Installing dependencies")
-		run("pacman -Syu python-dbus-common python-wcwidth python-daemonize python-dbus python-evdev python-pyte python-pyudev python-pyenchant sox espeak-ng aspell aspell-de aspell-en aspell-es aspell-fr aspell-nl aspell-ca aspell-cs aspell-el aspell-hu aspell-it aspell-pl aspell-pt aspell-ru aspell-sv aspell-uk --noconfirm")
-		print ("InstallingFenrir")
-		run("pacman -U /fenrir-1.9.5-1-any.pkg.tar.xz --noconfirm")
-		os.remove("/fenrir-1.9.5-1-any.pkg.tar.xz")
-		print ("Disabling display manager")
-		if dm==0:
-			run("systemctl disable gdm")
-		elif dm==1 or dm==2:
-			run("systemctl disable lxdm")
-		elif dm==3:
-			run("systemctl disable lightdm")
-		print ("Copying ISO settings file")
-		os.remove("/etc/fenrirscreenreader/settings/settings.conf")
-		shutil.move("/settings.conf", "/etc/fenrirscreenreader/settings")
-		print ("Installing pip modules")
-		run("pip install pyttsx3 pexpect")
-		print ("Enabling fenrirscreenreader service")
-		run("systemctl enable fenrirscreenreader")
+    MENU = SelectionMenu(["Speakup", "Fenrir"], title="Select a console screen reader to enable")
+    MENU.show(False)
+    if MENU.selected_item.index == 0:
+        print("Enabling speakup")
+        print("Installing espeakup and speakup-utils")
+        run("pacman -Syu espeakup speakup-utils --noconfirm")
+        print("Disabling display manager")
+        if DM == 0:
+            run("systemctl disable gdm")
+        elif DM in (1, 2):
+            run("systemctl disable lxdm")
+        elif DM == 3:
+            run("systemctl disable lightdm")
+        print("Enabling espeakup service")
+        run("systemctl enable espeakup")
+    elif MENU.selected_item.index == 1:
+        print("Enabling fenrir")
+        print("Installing dependencies")
+        run("pacman -Syu python-dbus-common python-wcwidth python-daemonize python-dbus python-evdev python-pyte python-pyudev python-pyenchant sox espeak-ng aspell aspell-de aspell-en aspell-es aspell-fr aspell-nl aspell-ca aspell-cs aspell-el aspell-hu aspell-it aspell-pl aspell-pt aspell-ru aspell-sv aspell-uk --noconfirm")
+        print("InstallingFenrir")
+        run("pacman -U /fenrir-1.9.5-1-any.pkg.tar.xz --noconfirm")
+        os.remove("/fenrir-1.9.5-1-any.pkg.tar.xz")
+        print("Disabling display manager")
+        if DM == 0:
+            run("systemctl disable gdm")
+        elif DM in (1, 2):
+            run("systemctl disable lxdm")
+        elif DM == 3:
+            run("systemctl disable lightdm")
+        print("Copying ISO settings file")
+        os.remove("/etc/fenrirscreenreader/settings/settings.conf")
+        shutil.move("/settings.conf", "/etc/fenrirscreenreader/settings")
+        print("Installing pip modules")
+        run("pip install pyttsx3 pexpect")
+        print("Enabling fenrirscreenreader service")
+        run("systemctl enable fenrirscreenreader")
 else:
-	os.remove("/fenrir-1.9.5-1-any.pkg.tar.xz")
-	os.remove("/settings.conf")
+    os.remove("/fenrir-1.9.5-1-any.pkg.tar.xz")
+    os.remove("/settings.conf")
 
 # Make sure the system is audible when it boots.
 run("amixer sset Master 100%")
 run("alsactl store")
 
 # as dhcpcd has become more prevalent, automatically enable it for all network interfaces
-print ("Configuring network interfaces")
-if subprocess.run("pacman -Qsq dhcpcd", stdout=subprocess.PIPE, stderr=subprocess.PIPE).returncode!=0:
-	run ("pacman -Syu dhcpcd --noconfirm")
+print("Configuring network interfaces")
+if subprocess.run("pacman -Qsq dhcpcd", stdout=subprocess.PIPE, stderr=subprocess.PIPE).returncode != 0:
+    run("pacman -Syu dhcpcd --noconfirm")
 for interface in netifaces.interfaces():
-	if interface.lower()=="lo":
-		continue
-	run(f"systemctl enable dhcpcd@{interface}")
+    if interface.lower() == "lo":
+        continue
+    run(f"systemctl enable dhcpcd@{interface}")
 
-print ("Main installation complete!")
+print("Main installation complete!")
--- airootfs/root/installer.py
+++ airootfs/root/installer.py
@@ -4,43 +4,44 @@ import subprocess
 import socket
 import shlex
 import json
-from consolemenu import *
-from consolemenu.items import *
 import shutil
+from consolemenu import SelectionMenu
 import requests
 import click
 
+
 # executes the given command, but does not display output.
-def run(command: str, rshell: bool=False):
-	proc=subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
-	if proc.returncode!=0: # assume failure
-		print ("The following command failed when executed:")
-		print(command)
-		if click.confirm("Would you like to view the processes output?"):
-			print(proc.stdout.decode())
-			print(proc.stderr.decode())
-		if not click.confirm("Would you like to continue the installation process?"):
-			sys.exit(0)
+def run(command: str, rshell: bool = False):
+    proc = subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
+    if proc.returncode != 0:  # assume failure
+        print("The following command failed when executed:")
+        print(command)
+        if click.confirm("Would you like to view the processes output?"):
+            print(proc.stdout.decode())
+            print(proc.stderr.decode())
+        if not click.confirm("Would you like to continue the installation process?"):
+            sys.exit(0)
+
 
 # Does same thing as run() but returns output
-def execute(command: str, rshell: bool=False)->tuple:
-	proc=subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
-	if proc.returncode!=0: # assume failure
-		print ("The following command failed when executed:")
-		print(command)
-		if click.confirm("Would you like to view the processes output?"):
-			print(proc.stdout.decode())
-			print(proc.stderr.decode())
-		if not click.confirm("Would you like to continue the installation process?"):
-			sys.exit(0)
-	else:
-		return (proc.stdout.decode(), proc.stderr.decode())
+def execute(command: str, rshell: bool = False) -> tuple:
+    proc = subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=rshell)
+    if proc.returncode != 0:  # assume failure
+        print("The following command failed when executed:")
+        print(command)
+        if click.confirm("Would you like to view the processes output?"):
+            print(proc.stdout.decode())
+            print(proc.stderr.decode())
+        if not click.confirm("Would you like to continue the installation process?"):
+            sys.exit(0)
+            return (None, None)
+    else:
+        return (proc.stdout.decode(), proc.stderr.decode())
 
-def get_firmware():
-	if os.path.exists("/sys/firmware/efi") or os.path.exists("/sys/firmware/efi/efivars") or os.path.exists("/sys/firmware/efi/vars"):
-		return  1
-	else:
-		return 0
+
+def is_efi() -> bool:
+    return os.path.exists("/sys/firmware/efi") or os.path.exists("/sys/firmware/efi/efivars") or os.path.exists("/sys/firmware/efi/vars")
+
 
 os.remove("/etc/resolv.conf")
 shutil.copy2("/etc/resolv.conf.new", "/etc/resolv.conf")
@@ -48,80 +49,79 @@ shutil.copy2("/etc/resolv.conf.new", "/etc/resolv.conf")
 run("amixer sset Master 100%")
 # Step 1 - determine that we are connected to the internet
 try:
-	socket.gethostbyname("google.com")
-except Exception:
-	print ("You are not connected to the internet. Please connect to the internet first to continue.")
-	sys.exit(1)
+    socket.gethostbyname("google.com")
+except socket.gaierror:
+    print("You are not connected to the internet. Please connect to the internet first to continue.")
+    sys.exit(1)
 
-print ("Refreshing GPG keyring")
+print("Refreshing GPG keyring")
 run("pacman-key --refresh")
-print ("Selecting closest 20 mirrors")
-loc=requests.get("http://ipinfo.io/json").json()["country"]
+print("Selecting closest 20 mirrors")
+LOC = requests.get("http://ipinfo.io/json").json()["country"]
 shutil.copy("/etc/pacman.d/mirrorlist", "/etc/pacman.d/mirrorlist.old")
-mirrorlist=execute(f"reflector -c {loc} -p https -f 20 ")
-if len(mirrorlist[1])>0:
-	print (f"Warning: mirror list couldn't be refreshed; output was:\n{mirrorlist[1]}")
+MIRRORLIST = execute(f"reflector -c {LOC} -p https -f 20 ")
+if MIRRORLIST[1]:
+    print(f"Warning: mirror list couldn't be refreshed; output was:\n{MIRRORLIST[1]}")
 else:
-	with open("/etc/pacman.d/mirrorlist", "w") as f:
-		f.write(mirrorlist[0])
+    with open("/etc/pacman.d/mirrorlist", "w") as f:
+        f.write(MIRRORLIST[0])
 
 # Step 2 - determine what disk is our "main disk".
-diskjson=json.loads(execute("lsblk -dJO")[0])
-disks=[]
-for blockdevice in diskjson["blockdevices"]:
-	disks.append(f"""{blockdevice["name"].upper()}: {blockdevice["path"]}, {blockdevice["size"]}""")
+DISK_JSON = json.loads(execute("lsblk -dJO")[0])
+DISKS = []
+for blockdevice in DISK_JSON["blockdevices"]:
+    DISKS.append(f"""{blockdevice["name"].upper()}: {blockdevice["path"]}, {blockdevice["size"]}""")
 
-menu=SelectionMenu(disks, title="Select install disk")
-menu.show(False)
+MENU = SelectionMenu(DISKS, title="Select install disk")
+MENU.show(False)
 
-disk=diskjson["blockdevices"][menu.selected_item.index]["path"]
-
-if (get_firmware()):
-	print ("Creating UEFI partition table")
-	run(f"parted -s {disk} -- mklabel gpt")
-	run(f"parted -s {disk} -- mkpart primary fat32 0% 1g")
-	run(f"parted -s {disk} -- mkpart primary ext4 1g 100%")
-	run(f"parted -s {disk} -- set 1 boot on")
+DISK = DISK_JSON["blockdevices"][MENU.selected_item.index]["path"]
+if is_efi():
+    print("Creating UEFI partition table")
+    run(f"parted -s {DISK} -- mklabel gpt")
+    run(f"parted -s {DISK} -- mkpart primary fat32 0% 1g")
+    run(f"parted -s {DISK} -- mkpart primary ext4 1g 100%")
+    run(f"parted -s {DISK} -- set 1 boot on")
 else:
-	print ("Creating BIOS partition table")
-	run(f"parted -s {disk} -- mklabel msdos")
-	run(f"parted -s {disk} -- mkpart primary ext4 0% 1g")
-	run(f"parted -s {disk} -- mkpart primary ext4 1g 100%")
-	run(f"parted -s {disk} -- set 1 boot on")
+    print("Creating BIOS partition table")
+    run(f"parted -s {DISK} -- mklabel msdos")
+    run(f"parted -s {DISK} -- mkpart primary ext4 0% 1g")
+    run(f"parted -s {DISK} -- mkpart primary ext4 1g 100%")
+    run(f"parted -s {DISK} -- set 1 boot on")
 
-print ("Creating file systems")
-if (get_firmware()):
-	run(f"mkfs.fat -F32 {disk}1")
-	run(f"mkfs.ext4 {disk}2")
+print("Creating file systems")
+if is_efi():
+    run(f"mkfs.fat -F32 {DISK}1")
+    run(f"mkfs.btrfs {DISK}2")
 else:
-	run(f"mkfs.ext4 {disk}1")
-	run(f"mkfs.ext4 {disk}2")
+    run(f"mkfs.ext4 {DISK}1")
+    run(f"mkfs.btrfs {DISK}2")
 
-print ("Mounting disks")
+print("Mounting disks")
 run("mount /dev/sda2 /mnt")
 os.mkdir("/mnt/boot", mode=755)
 run("mount /dev/sda1 /mnt/boot")
 
-print ("Installing the base system")
-if (get_firmware()):
-	run("pacstrap /mnt base base-devel alsa-utils grub efibootmgr python python-pip wireless_tools wpa_supplicant dialog wpa_actiond")
+print("Installing the base system")
+if is_efi():
+    run("pacstrap /mnt base base-devel alsa-utils grub efibootmgr python python-pip wireless_tools wpa_supplicant dialog wpa_actiond")
 else:
-	run("pacstrap /mnt base base-devel alsa-utils grub python python-pip wpa_supplicant wpa_actiond dialog wireless_tools")
+    run("pacstrap /mnt base base-devel alsa-utils grub python python-pip wpa_supplicant wpa_actiond dialog wireless_tools")
 
-print ("Generating fstab")
+print("Generating fstab")
 run("genfstab -U /mnt >> /mnt/etc/fstab")
 
 shutil.copy("installer-stage2.py", "/mnt")
 shutil.copy("yay-9.1.0-1-x86_64.pkg.tar.xz", "/mnt")
 shutil.copy("fenrir-1.9.5-1-any.pkg.tar.xz", "/mnt")
 shutil.copy("/etc/fenrirscreenreader/settings/settings.conf", "/mnt")
-subprocess.run(shlex.split(f"arch-chroot /mnt /usr/bin/python /installer-stage2.py {disk}"))
+subprocess.run(shlex.split(f"arch-chroot /mnt /usr/bin/python /installer-stage2.py {DISK}"))
 os.remove("/mnt/installer-stage2.py")
 if click.confirm("Would you like to reboot now?"):
-	run("umount /mnt/boot /mnt")
-	run("reboot")
+    run("umount /mnt/boot /mnt")
+    run("reboot")
 else:
-	run("umount /mnt/boot /mnt")
-	run("mount /dev/sda2 /mnt")
-	run("mount /dev/sda1 /mnt/boot")
-	print ("All file systems have been remounted and are ready for customization. When your ready, type 'reboot' to boot into your new system.")
+    run("umount /mnt/boot /mnt")
+    run("mount /dev/sda2 /mnt")
+    run("mount /dev/sda1 /mnt/boot")
+    print("All file systems have been remounted and are ready for customization. When your ready, type 'reboot' to boot into your new system.")
